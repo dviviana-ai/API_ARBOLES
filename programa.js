bc5b5988421b92a167af6519353c9ae1a30f90ef
Viviana <dvivianapaty@gmail.com>
2026-02-13 20:09:50 -0500

    v2

--- programa.js
+++ programa.js
@@ -1,10 +1,12 @@
 let url= "http://127.0.0.1:8000/getjson";
 
 
-async function crearPeticion(){
-    let myAPI = url + "?"
+document. getElementById("opcion").addEventListener("change", async function () {
+    
+    let localidadSeleccionada = this.value;
+    let myAPI = url + "?a=" + localidadSeleccionada;
     let response = await fetch(myAPI);
     let datos= await response.json();
 
     console.log(datos);
-}
+});
